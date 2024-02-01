ffa353f10fe3681573596bdc3d95f4955f155cc8
jojoldu <jojoldu@gmail.com>
2024-02-01 22:23:07 +0900

    중간 리팩토링 단계 추가

--- dop-frontend-next14/app/cart2/page.tsx
+++ dop-frontend-next14/app/cart2/page.tsx
@@ -4,11 +4,29 @@ import "reflect-metadata";
 import { Product, ProductType } from "@/app/product/Product";
 import React, { useState } from "react";
 import Link from "next/link";
-import { container } from "tsyringe";
-import { CartProbe } from "@/app/cart2/probe/CartProbe";
+import { gtmAnalytics } from "@/app/utils/GtmAnalytics";
+import mixpanel from "mixpanel-browser";
+
+// 개발할때마다, 테스트코드를 수행할때마다 메트릭 지표를 보낼 것인가?
+// 메트릭이 의도한대로 전송된다는 것은 어떻게 검증할 것인가?
+function sendRemoveMetric(product: Product) {
+  if (product.type === ProductType.FOOD) {
+    gtmAnalytics.track("click_remove_cart_food");
+  } else if (product.type === ProductType.BOOK) {
+    gtmAnalytics.track("click_remove_cart_book");
+  } else if (product.type === ProductType.CLOTHING) {
+    gtmAnalytics.track("click_remove_cart_clothing");
+  }
+
+  mixpanel.track("product_removed_cart", {
+    productId: product.id,
+    name: product.name,
+    price: product.price,
+    productType: product.type
+  });
+}
 
 export default function CartPage() {
-  const probe = container.resolve(CartProbe);
 
   // 장바구니 상태
   const [cart, setCart] = useState<Product[]>([
@@ -18,8 +36,7 @@ export default function CartPage() {
 
   const removeFromCart = (product: Product) => {
     setCart(cart.filter(p => p.id !== product.id));
-
-    probe.remove(product);
+    sendRemoveMetric(product);
   };
 
   return (
---
+++ dop-frontend-next14/app/cart3/page.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import "reflect-metadata";
+import { Product, ProductType } from "@/app/product/Product";
+import React, { useState } from "react";
+import Link from "next/link";
+import { container } from "tsyringe";
+import { CartProbe } from "@/app/cart3/probe/CartProbe";
+
+export default function CartPage3() {
+  const probe = container.resolve(CartProbe);
+
+  // 장바구니 상태
+  const [cart, setCart] = useState<Product[]>([
+    Product.of('1', 'Product 1', 10, ProductType.BOOK),
+    Product.of('2', 'Product 2', 20, ProductType.FOOD),
+  ]);
+
+  const removeFromCart = (product: Product) => {
+    setCart(cart.filter(p => p.id !== product.id));
+
+    probe.remove(product);
+  };
+
+  return (
+    <div>
+      <h1>장바구니</h1>
+      <Link href="/">Home 돌아가기</Link>
+      <ul>
+        {cart.map(product => (
+          <li key={product.id}>
+            {product.name} - ${product.price} |
+            <button onClick={() => removeFromCart(product)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
