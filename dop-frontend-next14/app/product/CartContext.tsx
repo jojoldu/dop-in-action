ab940fdebc79547a6649b0764489d0a6ccd5fc04
jojoldu <jojoldu@gmail.com>
2024-02-02 21:10:28 +0900

    Context 추가

---
+++ dop-frontend-next14/app/product/CartContext.tsx
@@ -0,0 +1,34 @@
+import { Product } from "@/app/product/Product";
+import { createContext, ReactNode, useContext, useState } from "react";
+
+interface CartContextType {
+  cart: Product[];
+  addToCart: (product: Product) => void;
+}
+
+const CartContext = createContext<CartContextType | undefined>(undefined);
+
+export const useCart = () => {
+  const context = useContext(CartContext);
+  if (context === undefined) {
+    throw new Error('useCart must be used within a CartProvider');
+  }
+  return context;
+};
+
+interface CartProviderProps {
+  children: ReactNode;
+}
+export const CartProvider: React.FC<CartProviderProps> = ({ children }) => {
+  const [cart, setCart] = useState<Product[]>([]);
+
+  const addToCart = (product: Product) => {
+    setCart((prevCart) => [...prevCart, product]);
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
